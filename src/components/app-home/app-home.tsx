28b71ae95e14009f4a085abd961b3b3a62020fac
Skylar Grammas <skylar.grammas@fullmeasureed.com>
2019-10-01 14:39:24 -0400

    something

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,10 +1,26 @@
 import { Component, h} from '@stencil/core';
+import _ from 'underscore';
 
 @Component({
   tag: 'app-home',
   styleUrl: 'app-home.css'
 })
 export class AppHome {
+  address = 'http://localhost:3000/users';
+  email: HTMLIonInputElement;
+  password: HTMLIonInputElement;
+  users: [];
+
+  async login() {
+    const response = await fetch(this.address);
+    const objects = await response.json();
+    this.users = objects.data;
+    debugger;
+    if (_.pluck(this.users, "email").includes(this.email)) {
+
+    }
+
+  }
 
   render() {
     return [
@@ -12,16 +28,17 @@ export class AppHome {
         <ion-grid style={{marginTop:"30vh"}}>
           <ion-row align-items-center={true} justify-content-around={true}>
             <ion-item color={"clear"} style={{width:"84vw", marginRight:"5vw"}}>
-              <ion-input placeholder={"Email Address"}></ion-input>
+              <ion-input placeholder={"Email Address"} ref={ (field) => this.email = field as HTMLIonInputElement }/>
+              
             </ion-item>
           </ion-row>
           <ion-row align-items-center={true} justify-content-around={true}>
             <ion-item color={"clear"} style={{width:"84vw", marginRight:"5vw"}}>
-              <ion-input placeholder={"Password"}></ion-input>
+              <ion-input placeholder={"Password"} ref={ (field) => this.password = field as HTMLIonInputElement }/>
             </ion-item>
           </ion-row>
           <ion-row align-items-center={true} justify-content-around={true} style={{marginTop:"6vh"}}>
-            <ion-button size="default" color="warning" style={{width:"80vw"}} expand={"full"}><ion-text color="dark">LOGIN</ion-text></ion-button>
+            <ion-button onClick={ () => this.login() } size="default" color="warning" style={{width:"80vw"}} expand={"full"}><ion-text color="dark">LOGIN</ion-text></ion-button>
           </ion-row>
           <ion-row align-items-center justify-content-around>
             <ion-button href="#/registration" size="default" style={{width:"80vw"}} expand={"full"}>LOGIN USING FACEBOOK</ion-button>

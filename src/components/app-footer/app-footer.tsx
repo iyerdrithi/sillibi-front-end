7b952c259f555e02dfe867dd2862a6b5ed22f96f
Logan Voorhees <loganvoorhees@cox.net>
2019-10-04 15:47:39 -0400

    footer icons coloring

--- src/components/app-footer/app-footer.tsx
+++ src/components/app-footer/app-footer.tsx
@@ -1,11 +1,16 @@
 import { Component, h } from '@stencil/core';
 import {AppRoot} from "../app-root/app-root";
+import {RouteService} from "../../services/route.service";
 
 @Component({
   tag: 'app-footer',
   styleUrl: 'app-footer.css'
 })
 export class AppFooter {
+
+  getColor(path) {
+    return RouteService.path().match(path) ? 'warning' : 'medium';
+  }
 
   render() {
     return (
@@ -16,25 +21,25 @@ export class AppFooter {
               <ion-col>
                 <ion-button color={'dark-purple'} icon-only item-end
                             onClick={() => AppRoot.route('/profile')}>
-                  <ion-icon name="contact" size='large' color={"medium"} />
+                  <ion-icon name="contact" size='large' color={this.getColor('profile')} />
                 </ion-button>
               </ion-col>
               <ion-col>
                 <ion-button color={'dark-purple'} icon-only item-end
                             onClick={() => AppRoot.route('/mycourses')}>
-                  <ion-icon name="document" size='large' color={"medium"}/>
+                  <ion-icon name="document" size='large' color={this.getColor('mycourses')}/>
                 </ion-button>
               </ion-col>
               <ion-col>
                 <ion-button color={'dark-purple'} icon-only item-end
                             onClick={() => AppRoot.route('/allassignments')}>
-                  <ion-icon name="paper" size='large' color={"medium"}/>
+                  <ion-icon name="paper" size='large' color={this.getColor('allassignments')}/>
                 </ion-button>
               </ion-col>
               <ion-col>
                 <ion-button color={'dark-purple'} icon-only item-end
                             onClick={() => AppRoot.route('/')}>
-                  <ion-icon name="more" size='large' color={"medium"}/>
+                  <ion-icon name="more" size='large' color={this.getColor('/')}/>
                 </ion-button>
               </ion-col>
             </ion-row>
